87b5bff8b3da82d0ae59e61abbbed084dde52064
nileshkashani <nileshkashani45@gmail.com>
2026-09-15 06:27:45 +0530

    first commit

---
+++ src/app/config/db.js
@@ -0,0 +1,6 @@
+    // import { createClient } from "@supabase/supabase-js";
+    // const db = () => {
+    //     createClient("https://zguudsqpprhxbwogbtvy.supabase.co/rest/v1/", import.meta.env.NEXT_PUBLIC_SUPHABASE_PUBLISHABLE_KEY)
+    //         .then(res => console.log(res))
+    //         .catch(err => console.log(err))
+    // }
